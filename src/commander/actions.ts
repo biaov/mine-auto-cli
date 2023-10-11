4975f6a63f5666fc3752682fddc287a48439f774
biaov <biaov@qq.com>
2023-10-11 18:06:55 +0800

    更新代码

--- src/commander/actions.ts
+++ src/commander/actions.ts
@@ -11,7 +11,7 @@ import { templateUrls, templateNames, installTools, toolCommands } from '@/confi
 import { SavePresetInfo, GetPresetInfo, FormatePreset } from '@/utils/functions'
 import { version } from '@/../package.json'
 import { PresetInfo, Template } from './types'
-import { gitCmds } from './cmds'
+import { gitCmds, buildCmds } from './cmds'
 
 const { iconError, iconSuccess, success, info, error, warning } = log
 
@@ -22,12 +22,13 @@ export const simplifyGit = async (desc = '更新代码') => {
   info()
   info(`${chalk.yellow('>>')} 开始依次执行命令...`)
   info()
-  // 执行命令
-  gitCmds.forEach(cmd => {
+  /**
+   * 执行命令
+   */
+  gitCmds(desc).forEach(cmd => {
     info(`开始执行 ${chalk.cyanBright(cmd)} 命令`)
     const spinner = ora({ text: '正在执行命令中...', color: 'yellow' })
     spinner.start()
-    cmd.includes('-m') && (cmd += ` ${desc}`)
     execSync(cmd, { cwd: process.cwd() })
     spinner.succeed(`${chalk.green(cmd)} 命令执行成功`)
     info()
@@ -40,8 +41,24 @@ export const simplifyGit = async (desc = '更新代码') => {
  * 简化 git 提交命令
  */
 export const simplifyBuild = async (cmd: string, desc = '更新代码') => {
-  console.log(cmd, desc)
-  return `npm run ${cmd}`
+  info()
+  info(`${chalk.yellow('>>')} 开始依次执行命令...`)
+  info()
+
+  /**
+   * 执行命令
+   */
+  buildCmds(cmd, desc).map(cmd => {
+    info(`开始执行 ${chalk.cyanBright(cmd)} 命令`)
+    const spinner = ora({ text: '正在执行命令中...', color: 'yellow' })
+    spinner.start()
+    execSync(cmd, { cwd: process.cwd() })
+    spinner.succeed(`${chalk.green(cmd)} 命令执行成功`)
+    info()
+  })
+
+  success('全部命令执行完成')
+  info()
 }
 
 /**
--- src/commander/cmds.ts
+++ src/commander/cmds.ts
@@ -1,4 +1,9 @@
 /**
  * git 命令集合
  */
-export const gitCmds: string[] = ['git add -A', 'git commit -m', 'git push']
+export const gitCmds = (desc: string) => ['git add -A', `git commit -m ${desc}`, 'git push']
+
+/**
+ * 打包的命令
+ */
+export const buildCmds = (suffix: string, desc: string) => [`npm run ${suffix}`, 'git add -A', `git commit -m ${desc}`, 'git push']
